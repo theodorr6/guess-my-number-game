f3ca0286a0c2f99f948c48a5d5b1e228b90fbdb1
Teodor Virjoghe <imhunterize@gmail.com>
2023-04-05 14:18:30 +0300

    update in JS + responsiveness
    cleaned JS code and added mobile responiveness

--- script.js
+++ script.js
@@ -34,21 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high! ⬆️';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low! ⬇️';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high! ⬆️' : 'Too low! ⬇️';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
